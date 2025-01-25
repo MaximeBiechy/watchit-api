7d46c8f789705cfc972ccd399183fe394e2ac103
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-01-25 18:54:13 +0100

    feat: add some errors

--- src/app/middlewares/errorHandler.ts
+++ src/app/middlewares/errorHandler.ts
@@ -1,23 +1,35 @@
 import { Request, Response, NextFunction } from 'express';
 import { BadRequest, HttpError } from 'express-openapi-validator/dist/framework/types.js';
 import logger from '../../shared/utils/logger.js';
+import { DatabaseError, NotFoundError } from '../../shared/errors/index.js';
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof HttpError) {
     logger.error(err, 'Openapi Error Validator');
-
-    return res.status(err.status || 500).json({
+    const statusCode = err.status || 500;
+    const response = {
       message: err.message,
       code: err?.constructor?.name || 'UnknownError',
-      ...(err instanceof BadRequest
-        ? {
-            errors: err.errors,
-          }
-        : {}),
-    });
+      ...(err instanceof BadRequest ? { errors: err.errors } : {}),
+    };
+
+    return res.status(statusCode).json(response);
   }
 
-  // ! Add other error handling logic here i guess :D
+  switch (true) {
+    case err instanceof DatabaseError:
+      return res.status(500).json({
+        message: err.message,
+        code: 'DatabaseError',
+      });
+    case err instanceof NotFoundError:
+      return res.status(404).json({
+        message: err.message,
+        code: 'NotFoundError',
+      });
+    default:
+      break;
+  }
 
   logger.error(err, 'Internal Server Error');
 
---
+++ src/shared/errors/index.ts
@@ -0,0 +1,4 @@
+import DatabaseError from './DatabaseError.js';
+import NotFoundError from './NotFoundError.js';
+
+export { DatabaseError, NotFoundError };
