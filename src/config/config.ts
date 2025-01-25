09c7e4ef78d8dd8d6c6770041abefaf7eaf2d720
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-01-25 18:53:46 +0100

    refactor: add environnement variable

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,5 +7,8 @@ export const config = {
   DB: {
     MONGO_IP: process.env.MONGO_IP || 'mongo',
     MONGO_PORT: process.env.MONGO_PORT || 27017,
+    MONGO_DB: process.env.MONGO_DB || 'watchit_dev',
+    MONGO_USER: process.env.MONGO_INITDB_ROOT_USERNAME || 'root',
+    MONGO_PASSWORD: process.env.MONGO_INITDB_ROOT_PASSWORD || 'password',
   },
 };
