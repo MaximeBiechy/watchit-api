83ab9456d3fdf5e0ee3264e700826f65719eb136
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-03-06 17:23:24 +0100

    feat: update OpenAPI specification to include status and message fields in responses and add search endpoint

--- src/interface/controllers/UsersController.ts
+++ src/interface/controllers/UsersController.ts
@@ -5,15 +5,16 @@ import { GetAllUsersUseCase } from '../../application/use-cases/index.js';
 import { UserDTO } from '../../domain/dtos/index.js';
 
 class UsersController {
-  constructor(@inject(TYPES.GetAllUsersUseCase) private getAllUsersUseCase: GetAllUsersUseCase) {}
+  constructor(@inject(TYPES.GetAllUsersUseCase) private getAllUsersUseCase: GetAllUsersUseCase) {
+  }
 
   async getAllUsers(req: Request, res: Response, next: NextFunction) {
     try {
       const users: UserDTO[] = await this.getAllUsersUseCase.execute();
       return res.status(200).json({
         status: 'success',
-        users,
         message: 'Users fetched successfully',
+        users,
       });
     } catch (error: any) {
       next(error);
